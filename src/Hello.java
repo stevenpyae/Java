d9569618d561ad1f08bdc19233c64d2b790d4570
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-02-18 15:55:58 +0800

    Variables

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         String name = "Steven";
         System.out.println("Hello, " + name);
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
