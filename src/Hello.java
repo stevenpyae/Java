49c7825d6b4ecb164e12b69f53265ad2af008da9
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-02-23 21:23:15 +0800

    Expressions

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
         int myFirstNumber = (10+5) + (2*10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
         System.out.println(myTotal);
 
