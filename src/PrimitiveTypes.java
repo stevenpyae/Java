d6494e191cdeac6b11d6d7b01ca533fcb84af033
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-03-22 13:43:27 +0800

    Expressions

---
+++ src/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+public class PrimitiveTypes {
+
+    public static void main(String[] args){
+    int myValue = 10000;
+    int myMinIntValue = Integer.MIN_VALUE;
+    int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
+}
+}
