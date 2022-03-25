347e82030b1f51a4b35ddedfcad0cc317be3fb92
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-03-25 16:03:20 +0800

    Primitive Types

--- src/PrimitiveTypes.java
+++ src/PrimitiveTypes.java
@@ -5,9 +5,10 @@ public class PrimitiveTypes {
     int myMinIntValue = Integer.MIN_VALUE;
     int myMaxIntValue = Integer.MAX_VALUE;
 
-        System.out.println("Integer Minimum Value = " + myMinIntValue);
-        System.out.println("Integer Maximum Value = " + myMaxIntValue);
-        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
-        System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
+    System.out.println("Integer Minimum Value = " + myMinIntValue);
+    System.out.println("Integer Maximum Value = " + myMaxIntValue);
+    System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
+    System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
+
 }
 }
